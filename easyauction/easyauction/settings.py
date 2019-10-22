5c7d7a4e063c6de952bc34c2400a75f30ac10fe2
Zion <zion.steiner@aggiemail.usu.edu>
2019-10-21 23:15:45 -0600

    Updated local master and then rebased this branch onto master
    Signed-off-by: Zion <zion.steiner@aggiemail.usu.edu>

--- easyauction/easyauction/settings.py
+++ easyauction/easyauction/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'easyauction.Apps.EasyauctionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
