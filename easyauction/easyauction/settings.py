62373513d626877536bc309e47e2627c845fabfa
Zion <zion.steiner@aggiemail.usu.edu>
2019-10-23 20:26:29 -0600

    Added temporary templates for login, signup, and home page. Setup basic login, logout, and signup functionality using built-in Django auth.
    Signed-off-by: Zion <zion.steiner@aggiemail.usu.edu>

--- easyauction/easyauction/settings.py
+++ easyauction/easyauction/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'easyauction.Apps.EasyauctionConfig',
+    'auction.apps.AuctionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'MDT'
+TIME_ZONE = 'America/Denver'
 
 USE_I18N = True
 
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home page
+LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
