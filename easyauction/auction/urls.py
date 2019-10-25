995c417e636d128f45f7a28705dd0afabc3d5ebf
Hannah <hannah.galli4897@aggiemail.usu.edu>
2019-10-25 11:13:10 -0600

    more work done on homepage. Still runs

--- easyauction/auction/urls.py
+++ easyauction/auction/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'auction'
 urlpatterns = [
     path('', views.login, name='login'),
-    path('home', views.home, name='home')
+    path('home', views.home, name='home'),
+    path('create_auction', views.create_auction, name='create_auction'),
+    path('enter_local_code', views.enter_local_code, name='enter_local_code'),
 ]
--- easyauction/auction/views.py
+++ easyauction/auction/views.py
@@ -8,3 +8,11 @@ def login(request):
 def home(request):
     return render(request, 'auction/home.html', context={})
 
+
+def create_auction(request):
+    return render(request, 'auction/create_auction.html', context={})
+
+
+def enter_local_code(request):
+    return render(request, 'auction/enter_local_code.html', context={})
+
