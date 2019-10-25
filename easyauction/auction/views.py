a4a41e9ff9ebf0ba1526217700c56e13411d1a92
Tristan Thomas <tristanlt034@aggiemail.usu.edu>
2019-10-25 14:45:54 -0600

    Added login, forgot password, and signup templates (no functionality)

--- easyauction/auction/views.py
+++ easyauction/auction/views.py
@@ -5,6 +5,18 @@ def login(request):
     return render(request, 'auction/login.html', context={})
 
 
+def forgot(request):
+    return render(request, 'auction/forgot.html', context={})
+
+
+def reset_password(request):
+    return render(request, 'auction/reset_password.html', context={})
+
+
+def signup(request):
+    return render(request, 'auction/signup.html', context={})
+
+
 def home(request):
     return render(request, 'auction/home.html', context={})
 
