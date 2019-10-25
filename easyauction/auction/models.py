88aad8e22628cad3579bd8faa7e1c54723e7f366
Garrett Johnson <itwinx@yahoo.com>
2019-10-24 21:21:27 -0600

    Fixed the random entry code generation.

--- easyauction/auction/models.py
+++ easyauction/auction/models.py
@@ -9,10 +9,13 @@ class User(models.Model):
 	def __str__(self):
 		return self.name
 
+def random_entry_code():
+	return randrange(100000)
+
 class Auction(models.Model):
 	name = models.CharField(max_length=200)
 	time_created = models.DateTimeField()
-	entry_code = models.IntegerField(default=randrange(100000))
+	entry_code = models.IntegerField(default=random_entry_code)
 	published = models.BooleanField(default=False)
 
 	def __str__(self):
