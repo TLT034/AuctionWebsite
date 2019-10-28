da6e4074ca95dcbef992704429ab98df9306ac32
Garrett Johnson <itwinx@yahoo.com>
2019-10-28 17:09:37 -0600

    Functional auction creation, but no auction page yet.

--- easyauction/auction/views.py
+++ easyauction/auction/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+
+from .forms import AuctionForm
+from .models import Auction
 
 
 def login(request):
@@ -6,8 +11,17 @@ def login(request):
 
 
 def home(request):
-    return render(request, 'auction/home.html', context={})
+	return render(request, 'auction/home.html', context={})
 
 
 def create_auction(request):
-	return render(request, 'auction/create_auction.html', context={})
+	if request.method == 'POST':
+		form = AuctionForm(request.POST)
+		if form.is_valid():
+			new_auction = Auction(name=form.cleaned_data['auction_name'], time_created=timezone.now())
+			new_auction.save()
+			return HttpResponseRedirect('home')
+	else:
+		form = AuctionForm()
+
+	return render(request, 'auction/create_auction.html', context={'form': form})
